bf6547c844f536ac25d0a412239aded9beee4ce2
SebasR1C0 <102307907+SebasR1C0@users.noreply.github.com>
2022-09-11 13:21:23 -0500

    Create util.h

---
+++ Clases/Semana 4/2/util.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef UTIL_H
+#define UTIL_H
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <windows.h>
+#include <vector>
+using namespace std;
+#define MAIN2
+#endif

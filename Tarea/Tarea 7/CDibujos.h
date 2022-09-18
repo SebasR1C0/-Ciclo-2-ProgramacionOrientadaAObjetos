d5e1cfa495b88ad4578f82185f81ae91adc4cd1b
SebasR1C0 <102307907+SebasR1C0@users.noreply.github.com>
2022-09-17 22:22:11 -0500

    Create CDibujos.h

---
+++ Tarea/Tarea 7/CDibujos.h
@@ -0,0 +1,58 @@
+#pragma once
+#pragma once
+#include "CBase.h"
+class CAlfa : public CBase
+{
+public:
+	CAlfa() : CBase(19, 5) {
+		x = 0;
+		dibujo.push_back("      _.---._");
+		dibujo.push_back("    .'       '.");
+		dibujo.push_back("_.-~===========~-._");
+		dibujo.push_back("(_________________)");
+		dibujo.push_back("    \\_______/");
+	}
+	~CAlfa();
+	void mover() {
+		validarMovimiento(0, dy * -1);
+	}
+};
+#pragma once
+#include "CBase.h"
+class CBeta : public CBase
+{
+public:
+	CBeta() : CBase(11, 3) {
+		x = (wConsola - ancho) / 2;
+		dibujo.push_back("  .---. ");
+		dibujo.push_back("_/__~0_\\_");
+		dibujo.push_back("(_________)");
+	}
+	~CBeta() {}
+	void mover() {
+		validarMovimiento(0, dy * -1);
+	}
+};
+#pragma once
+#include "CBase.h"
+class CGamma : public CBase
+{
+public:
+	CGamma() : CBase(17, 10) {
+		x = (wConsola - ancho);
+		dibujo.push_back("      .");
+		dibujo.push_back("      |");
+		dibujo.push_back("   .-\"^\"-.");
+		dibujo.push_back("  /_....._\\");
+		dibujo.push_back("  .-\"` `\"-.  ");
+		dibujo.push_back("( ooo ooo ooo ) ");
+		dibujo.push_back("'-.,_________,.-'");
+		dibujo.push_back("     / \\  ");
+		dibujo.push_back("    _/ \\_  ");
+		dibujo.push_back("    `\"` `\"`  ");
+	}
+	~CGamma() {}
+	void mover() {
+		validarMovimiento(0, dy * -1);
+	}
+};

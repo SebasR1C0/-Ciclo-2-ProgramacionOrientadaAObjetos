a007a5bac5ff7723c4310eab6ef6730e12ab1db0
SebasR1C0 <102307907+SebasR1C0@users.noreply.github.com>
2022-09-17 21:41:10 -0500

    Create CBase.h

---
+++ Talleres/Semana 5/Ejercicio 2/CBase.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#define wConsola 110
+#define hConsola 30
+using namespace std;
+using namespace System;
+using namespace System::Drawing;
+class CBase{
+public:
+	CBase(int pancho, int palto){
+		ancho = pancho;
+		alto = palto;
+		eliminar = false;
+		y = 0;
+		do {
+			dx = rand() % 3 - 1;
+			dy = rand() % 3 - 1;
+		} while (dx == 0 && dy == 0);
+	}
+	~CBase() {}
+	void borrar() {
+		for (int i = 0; i < alto; i++)
+			for (int j = 0; j < ancho; j++) {
+				Console::SetCursorPosition(x + j, y + i);
+				cout << " ";
+			}
+	}
+	void dibujar() {
+		for (int i = 0; i < dibujo.size(); i++) {
+			Console::SetCursorPosition(x, y + i);
+			cout << dibujo[i];
+		}
+	}
+	virtual void mover() {
+		validarMovimiento(dx * -1, dy * -1);
+	}
+	void validarMovimiento(int pdx, int pdy) {
+		if (x + dx < 0 || x + dx + ancho > wConsola) dx = pdx;
+		if (y + dy < 0 || y + dy + alto > hConsola) dy = pdy;
+		x += dx;
+		y += dy;
+	}
+
+	int getdy() { return dy;}
+	int getdx() { return dx;}
+	Rectangle getRec() {
+		return Rectangle(x, y, ancho, alto);
+	}
+	bool getEliminar() { return eliminar; }
+	void setEliminar() { eliminar = true; }
+protected:
+	int x, y, dx, dy, ancho, alto, tipo;
+	bool eliminar;
+	vector<string> dibujo;
+};

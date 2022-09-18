5a74f736a03cae0b7086bdf9d7c45a4ec83544ff
SebasR1C0 <102307907+SebasR1C0@users.noreply.github.com>
2022-09-18 12:37:37 -0500

    Update main.cpp

--- Tarea/Tarea 7/main.cpp
+++ Tarea/Tarea 7/main.cpp
@@ -1,11 +1,13 @@
 #include "Dino.h"
 #include "CControladora.h"
 #include "CControlador1.h"
+#include "CManejo.h"
 void menu(int& opcion) {
 	do {
 		cout << "1. Dibujar dinosaurio" << endl;
 		cout << "2. Atrapar fantasmas" << endl;
 		cout << "3. Comenzar una invasion" << endl;
+		cout << "4. Raton atrapa queso" << endl;
 		cout << "Ingresa una opcion: ";
 		cin >> opcion;
 	} while (opcion < 1 || opcion>6);
@@ -18,15 +20,16 @@ int main() {
 	Console::CursorVisible = false;
 	CControladora* objB = new CControladora();
 	CControlador1* objC = new CControlador1();
-	Dinosaurio1* obj = new Dinosaurio1();
+	Dinosaurio1* objA = new Dinosaurio1();
+	CManejo* objD = new CManejo();
 	menu(opcion);
 	switch (opcion) {
 	case 1:
 		while (true) {
-			obj->Mostrar();
+			objA->Mostrar();
 			_sleep(200);
-			obj->Borrar();
-			obj->Mover();
+			objA->Borrar();
+			objA->Mover();
 		}
 		break;
 	case 2: 
@@ -43,6 +46,13 @@ int main() {
 		objC->juego();
 		getch();
 		break;
-	case 4:
+	case 4: 
+		cout << "Presione espacio para agregar un raton"<<endl;
+		cout << "Presione cualquier otra tecla diferente a espacio y X para agregar un queso"<<endl;
+		cout << "Presione X para acabar con el juego" << endl;
+		system("cls");
+		objD->juego();
+		getch();
+		break;
 	}
 }

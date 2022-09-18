8826ad9ff18f1074e06b9790988d592e8ad9f40e
SebasR1C0 <102307907+SebasR1C0@users.noreply.github.com>
2022-09-18 12:21:04 -0500

    Create CQueso.h

---
+++ Talleres/Semana 4/Ejercicio 2/CQueso.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+#define anchoConsola 110
+#define altoConsola 30
+#define Rectangle System::Drawing::Rectangle
+using namespace std;
+using namespace System;
+class CQueso
+{
+public:
+    CQueso() {
+        ancho = 11;
+        alto = 7;
+        x = rand() % (anchoConsola - ancho);
+        y = rand() % (altoConsola - alto);
+        color = ConsoleColor(rand() % 16);
+    }
+    ~CQueso(){}
+    void dibujar() {
+        Console::SetCursorPosition(x, y);
+        cout << "     ___";
+        Console::SetCursorPosition(x, y + 1);
+        cout << "  .'o O'-._";
+        Console::SetCursorPosition(x, y + 2);
+        cout << " / O o_.-`|";
+        Console::SetCursorPosition(x, y + 3);
+        cout << "/O_.-'  O |";
+        Console::SetCursorPosition(x, y + 4);
+        cout << "| o   o .-`";
+        Console::SetCursorPosition(x, y + 5);
+        cout << "   |o O_.-'";
+        Console::SetCursorPosition(x, y + 6);
+        cout << "     '--`";
+    }
+    void borrar() {
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0; j < ancho; j++) {
+                Console::SetCursorPosition(x + j, y + i);
+                    cout << " ";
+            }
+        }
+    }
+    int getX() {
+        return x;
+    }
+    int getY() {
+        return y;
+    }
+    Rectangle getRectangle() {
+        return Rectangle(x, y, ancho, alto);
+    }
+private:
+    int x, y, ancho, alto;
+    ConsoleColor color;
+};
